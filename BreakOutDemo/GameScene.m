a3f97e28e0383a3dba76d6cf5c60178dd059e18c
Gu Han <guhan@gus-macbook-pro.local>
2017-07-03 17:01:06 -0700

    Remove second ball, add blocks and when ball hit a block, the block will be break.

--- BreakOutDemo/GameScene.m
+++ BreakOutDemo/GameScene.m
@@ -12,7 +12,12 @@
 
 static const CGFloat kTrackPointsPerSecond = 1000;
 
-@interface GameScene ()
+static const uint32_t category_fence  = 0x1 << 3;
+static const uint32_t category_paddle = 0x1 << 2;
+static const uint32_t category_block  = 0x1 << 1;
+static const uint32_t category_ball   = 0x1 << 0;
+
+@interface GameScene () <SKPhysicsContactDelegate>
 
 @property (nonatomic, strong, nullable) UITouch *motivatingTouch;
 
@@ -25,11 +30,29 @@ static const CGFloat kTrackPointsPerSecond = 1000;
 
 - (void)didMoveToView:(SKView *)view {
   // Setup your scene here
+  self.name = @"Fence";
   self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+  self.physicsBody.categoryBitMask = category_fence;
+  self.physicsBody.collisionBitMask = 0x0;
+  self.physicsBody.contactTestBitMask = 0x0;
+  
+  
+  self.physicsWorld.contactDelegate = self;
+  
+  
+  
+  
+  
+  
+  
   SKSpriteNode *ball1 = [SKSpriteNode spriteNodeWithImageNamed:@"ball.png"];
+  ball1.name = @"Ball";
+  ball1.position = CGPointMake(0, 0);
+  ball1.zPosition = 1;
+  
+  
   ball1.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball1.size.width/2];
   ball1.physicsBody.dynamic = YES;
-  ball1.position = CGPointMake(100, self.size.height/2);
   ball1.physicsBody.friction = 0.0;
   ball1.physicsBody.restitution = 1.0;
   ball1.physicsBody.linearDamping = 0.0;
@@ -37,18 +60,12 @@ static const CGFloat kTrackPointsPerSecond = 1000;
   ball1.physicsBody.allowsRotation = NO;
   ball1.physicsBody.mass = 1.0;
   ball1.physicsBody.velocity = CGVectorMake(200.0, 200.0);
-  
-  SKSpriteNode *ball2 = [SKSpriteNode spriteNodeWithImageNamed:@"ball.png"];
-  ball2.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball2.size.width/2];
-  ball2.physicsBody.dynamic = YES;
-  ball2.position = CGPointMake(300, self.size.height/2);
-  ball2.physicsBody.friction = 0.0;
-  ball2.physicsBody.restitution = 1.0;
-  ball2.physicsBody.linearDamping = 0.0;
-  ball2.physicsBody.angularDamping = 0.0;
-  ball2.physicsBody.allowsRotation = NO;
-  ball2.physicsBody.mass = 1.0;
-  ball2.physicsBody.velocity = CGVectorMake(0.0, 200.0);
+  ball1.physicsBody.affectedByGravity = NO;
+  ball1.physicsBody.categoryBitMask = category_ball;
+  ball1.physicsBody.collisionBitMask = category_fence | category_ball | category_block | category_paddle;
+  ball1.physicsBody.contactTestBitMask = category_fence | category_block;
+  ball1.physicsBody.usesPreciseCollisionDetection = YES;
+
   
   
   SKSpriteNode *paddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle.png"];
@@ -63,20 +80,48 @@ static const CGFloat kTrackPointsPerSecond = 1000;
   paddle.physicsBody.allowsRotation = NO;
   paddle.physicsBody.mass = 1.0;
   paddle.physicsBody.velocity = CGVectorMake(0.0, 0.0);
+  paddle.physicsBody.categoryBitMask = category_paddle;
+  paddle.physicsBody.collisionBitMask = 0x0;
+  paddle.physicsBody.contactTestBitMask = category_ball;
+  paddle.physicsBody.usesPreciseCollisionDetection = YES;
+  
   
   [self addChild:ball1];
-  [self addChild:ball2];
   [self addChild:paddle];
   
-  CGPoint ball1Anchor = CGPointMake(ball1.position.x, ball1.position.y);
-  CGPoint ball2Anchor = CGPointMake(ball2.position.x, ball2.position.y);
-  SKPhysicsJointSpring *joint = [SKPhysicsJointSpring jointWithBodyA:ball1.physicsBody
-                                                               bodyB:ball2.physicsBody
-                                                             anchorA:ball1Anchor
-                                                             anchorB:ball2Anchor];
-  joint.damping = 0.0;
-  joint.frequency = 1.5;
-  [self.scene.physicsWorld addJoint:joint];
+  SKSpriteNode *node = [SKSpriteNode spriteNodeWithColor:[UIColor greenColor] size:CGSizeMake(100, 30)];
+  CGFloat kBlockWidth = node.size.width;
+  CGFloat kBlockHeight = node.size.height;
+  CGFloat kBlockHorizSpace = 20.0f;
+  int kBlockPerRow = (self.size.width / (kBlockWidth + kBlockHorizSpace));
+  
+  
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < kBlockPerRow; i++) {
+      node = [SKSpriteNode spriteNodeWithColor:[UIColor greenColor] size:CGSizeMake(100, 30)];
+      node.name = @"Block";
+      node.position = CGPointMake(-self.size.width / 2 +  kBlockHorizSpace/2 + kBlockWidth/2 + i*(kBlockWidth+kBlockHorizSpace), self.size.height/2 - 30 - kBlockHeight/2 - j*(1.5*kBlockHeight));
+      node.zPosition = 1;
+      node.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:node.size center:CGPointMake(0, 0)];
+      node.physicsBody.dynamic = NO;
+      node.physicsBody.friction = 0.0;
+      node.physicsBody.restitution = 0.0;
+      node.physicsBody.linearDamping = 0.0;
+      node.physicsBody.angularDamping = 0.0;
+      node.physicsBody.allowsRotation = NO;
+      node.physicsBody.mass = 1.0;
+      node.physicsBody.velocity = CGVectorMake(0, 0);
+      node.physicsBody.categoryBitMask = category_block;
+      node.physicsBody.collisionBitMask = 0x0;
+      node.physicsBody.contactTestBitMask = category_ball;
+      node.physicsBody.usesPreciseCollisionDetection = NO;
+      [self addChild:node];
+      
+    }
+  }
+  
+
+  
   
   
   
@@ -128,6 +173,50 @@ static const CGFloat kTrackPointsPerSecond = 1000;
   NSTimeInterval duration = ABS(xPos - node.position.x) / kTrackPointsPerSecond;
   [node runAction:[SKAction moveToX:xPos duration:duration]];
   
+}
+
+- (void)update:(NSTimeInterval)currentTime {
+  static const int kMaxSpeed = 1500;
+  static const int kMinSpeed = 400;
+  
+  SKNode *ball1 = [self childNodeWithName:@"Ball"];
+  float dx = (ball1.physicsBody.velocity.dx) / 2;
+  float dy = (ball1.physicsBody.velocity.dy) / 2;
+  float speed = sqrt(dx*dx + dy*dy);
+  if (speed > kMaxSpeed) {
+    ball1.physicsBody.linearDamping += 0.1;
+  } else if (speed < kMinSpeed) {
+    ball1.physicsBody.linearDamping -= 0.1;
+  } else {
+    ball1.physicsBody.linearDamping = 0.0;
+  }
+  
+}
+
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+  NSString *nameA = contact.bodyA.node.name;
+  NSString *nameB = contact.bodyB.node.name;
+  if (([nameA containsString:@"Fence"] && [nameB containsString:@"Ball"]) ||
+       ([nameA containsString:@"Ball"] && [nameB containsString:@"Fence"])) {
+    NSLog(@"%f", contact.contactPoint.y);
+    if (contact.contactPoint.y < -self.size.height/2 + 100) {
+      SKView * skView = (SKView *)self.view;
+      [self removeFromParent];
+      GameOver *scene = [GameOver nodeWithFileNamed:@"GameOver"];
+      scene.scaleMode = SKSceneScaleModeFill;
+      [skView presentScene:scene];
+    }
+  } else if(([nameA containsString:@"Block"] && [nameB containsString:@"Ball"]) ||
+            ([nameA containsString:@"Ball"] && [nameB containsString:@"Block"])) {
+    if ([nameA containsString:@"Block"]) {
+      [contact.bodyA.node removeFromParent];
+    } else {
+      [contact.bodyB.node removeFromParent];
+    }
+    
+  } else {
+    NSLog(@"%@ %@", nameA, nameB);
+  }
 }
 
 @end
